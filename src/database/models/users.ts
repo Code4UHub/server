cfbd877d3053119e57f2b7ce300ca94f70882dee
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-14 00:00:26 -0600

    Created student type

--- src/database/models/users.ts
+++ src/database/models/users.ts
@@ -1,37 +1,31 @@
-import { Table, Column, Model, DataType } from "sequelize-typescript";
+import { Table, Column, Model, DataType } from 'sequelize-typescript'
 
-@Table({ tableName: 'student'})
+@Table({ tableName: 'student' })
 export class Student extends Model {
   @Column({
     type: DataType.STRING,
     primaryKey: true,
     allowNull: true
   })
-  student_id: string;
+  student_id: string
 
   @Column({
     type: DataType.STRING
   })
-  first_name: string;
-  
-  @Column({
-    type: DataType.STRING
-  })
-  last_name: string;
-  
-  @Column({
-    type: DataType.STRING
-  })
-  email: string;
-  
-  @Column({
-    type: DataType.STRING
-  })
-  password: string;
+  first_name: string
 
-  // public student_id!: number;
-  // public first_name!: string;
-  // public last_name!: string;
-  // public email!: string;
-  // public password!: string;
+  @Column({
+    type: DataType.STRING
+  })
+  last_name: string
+
+  @Column({
+    type: DataType.STRING
+  })
+  email: string
+
+  @Column({
+    type: DataType.STRING
+  })
+  password: string
 }
---
+++ src/types/user.type.ts
@@ -0,0 +1,7 @@
+export type StudentType = {
+  student_id: string
+  first_name: string
+  last_name: string
+  email: string
+  password: string
+}
