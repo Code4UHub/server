debe1e2f82666a60949dc0404bc7152baa26bbb8
José Ángel González <77761803+JAngelGC@users.noreply.github.com>
2023-04-14 00:02:18 -0600

    Feature/signup (#17)
    * Renamed queries for users
    
    * Created createUser query
    
    * Added route for post
    
    * Created student type

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,28 @@
 // const pg = require('./connection')
 
 import { createDb, authenticateDb } from './connection'
-import { listStudents, searchStudent } from './query/users'
+import { selectStudents, selectStudent } from './query/users'
+import { StudentType } from './../types/user.type'
 
 const test = async () => {
   const sequelize = await createDb()
   const validConnection = await authenticateDb(sequelize)
   if (validConnection) {
     // console.log("Connected");
-    const res = await searchStudent('a00000001@tec.com', 'Abc123456')
+    // const res = await searchStudent('a00000001@tec.com', 'Abc123456')
+    // const newStudent: StudentType = {
+    //   student_id: 'A01552274',
+    //   first_name: 'Juan',
+    //   last_name: 'Rulfo',
+    //   email: 'a01552274@tec.com',
+    //   password: '12345'
+    // }
+
+    // const createRes = await postUser(newStudent)
+
     // const res = await listStudents()
     // console.log(res)
-    // await searchStudent()
+    const res = await selectStudent('a00000001@tec.com', 'Abc123456')
     console.log(res)
     sequelize.close()
   }
--- src/database/query/users.ts
+++ src/database/query/users.ts
@@ -1,6 +1,7 @@
 import { Student } from './../models/users'
+import { StudentType } from '../../types/user.type'
 
-export const listStudents = async () => {
+export const selectStudents = async () => {
   try {
     const students = await Student.findAll({
       raw: true
@@ -16,24 +17,42 @@ export const listStudents = async () => {
   }
 }
 
-export const searchStudent = async (email: string, password: string) => {
+export const selectStudent = async (email: string, password?: string) => {
   try {
-    const student = await Student.findAll({
-      // attributes: ['first_name', 'last_name', 'email'],
-      raw: true,
-      where: {
-        email: email,
-        password: password
-      }
-    })
-    if (student.length < 1) {
-      return 'Account does not exist'
+    if (password) {
+      const student = await Student.findAll({
+        // attributes: ['first_name', 'last_name', 'email'],
+        raw: true,
+        where: {
+          email: email,
+          password: password
+        }
+      })
+      return student
     } else {
-      //   return JSON.stringify(student)
+      const student = await Student.findAll({ raw: true, attributes: ['email'], where: { email: email } })
       return student
     }
   } catch (e) {
     // throw new Error("MY ERROR")
     throw e
+  }
+}
+
+export const createUser = async (student: StudentType) => {
+  try {
+    const user = await selectStudent(student.email)
+    const userExists = user.length > 0 ? true : false
+    console.log(userExists)
+    if (!userExists) {
+      const res = await Student.create(student)
+      console.log('User succcesfully created')
+      return res
+    } else {
+      return 'User already exists'
+    }
+  } catch (e) {
+    // throw e
+    return 'Couldnt create user'
   }
 }
