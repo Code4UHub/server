def43586d2e6e5214ca39843c8cbec10f9620878
diaz <carlosd2mil@gmail.com>
2023-04-17 23:29:54 -0600

    Testing queries

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,9 @@
 // const pg = require('./connection')
 
 import { getTeachers } from '../controllers/teacherController'
+import { StudentType } from '../types/student.type'
 import { createDb, authenticateDb } from './connection'
-import { selectStudent } from './query/student'
+import { createStudent, selectStudent, selectStudents } from './query/student'
 import { selectTeacher, selectTeachers } from './query/teacher'
 
 const test = async () => {
@@ -13,11 +14,21 @@ const test = async () => {
 
     // const res = await listStudents()
     // console.log(res)
-    // const email = 'a00000001@tec.com'
-    // const pwd = 'Abc123456'
+    const email = 'a00000017@tec.mx'
+    const pwd = 'Abc123456'
     // const email = 'l00000001@tec.com'
     // const pwd = 'Abc123456'
-    // let res
+    const student: StudentType = {
+      student_id: 'a00000017',
+      first_name: 'My Name',
+      last_name: 'My LastName',
+      email: email,
+      password: pwd
+    }
+    const res = await createStudent(student)
+    // const res = await selectStudents()
+    console.log(res)
+    // console.log(res.length)
 
     // if (email[0] == 'a') {
     //   res = await selectStudent(email, pwd)
@@ -35,9 +46,6 @@ const test = async () => {
     //   }
     // }
 
-    const res = await selectTeachers()
-
-    console.log(res)
     sequelize.close()
   }
 }
