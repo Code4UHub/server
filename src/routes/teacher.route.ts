607986bc2764fbbebe7e6c26062ca4629f9542b9
diaz <carlosd2mil@gmail.com>
2023-04-20 12:43:14 -0600

    POST /teacher

--- src/routes/teacher.route.ts
+++ src/routes/teacher.route.ts
@@ -3,7 +3,6 @@ import * as teacherController from '../controllers/teacher.controller'
 
 export const router: Router = Router()
 
-router.route('/teacher').get(teacherController.getTeachers)
-// .post(teacherController.postTeacher)
+router.route('/teacher').get(teacherController.getTeachers).post(teacherController.postTeacher)
 
 router.route('/teacher/:email&:pwd').get(teacherController.getTeacher)
