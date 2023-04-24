3cc8705ac241153042b7d1fe686bc36df59653e6
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-23 19:24:49 -0600

    Added Bearer to token

--- src/utils/jwt-sign.ts
+++ src/utils/jwt-sign.ts
@@ -7,7 +7,7 @@ export const generateToken = (user_id: string): string => {
   try {
     const secret = process.env.JWT_SECRET as Secret
     const token = jwt.sign({ user_id: user_id }, secret, { expiresIn: '7d' })
-    return token
+    return `Bearer ${token}`
   } catch (e) {
     throw e
   }
