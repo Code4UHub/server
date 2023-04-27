6ec7a3dbedd1383d302be960922679533ed43231
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-26 20:35:51 -0600

    Added docs folder

--- src/routes/class.route.ts
+++ src/routes/class.route.ts
@@ -5,8 +5,7 @@ export const router: Router = Router()
 
 router.route('/class/all').get(classController.getClasses) //.post(classController.postController)
 router.route('/class/:id').get(classController.getClass)
-router.route('/class/register').post(classController.postClass)
-
+router.route('/class/create').post(classController.postClass)
 
 router.route('/class/register').post(classController.postRegisterStudent)
 router.route('/class/:id/students').get(classController.getStudentsByClass)
