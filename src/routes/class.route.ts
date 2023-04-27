fea44ed678db3fc0047e4eda013dee5fc85ca444
diaz <carlosd2mil@gmail.com>
2023-04-26 21:52:57 -0600

    Dev up-to-date

--- src/routes/class.route.ts
+++ src/routes/class.route.ts
@@ -3,8 +3,8 @@ import * as classController from '../controllers/class.controller'
 
 export const router: Router = Router()
 
-router.route('/class/all').get(classController.getClasses) //.post(classController.postController)
-router.route('/class/:id').get(classController.getClass)
+router.route('/class').get(classController.getClasses) //.post(classController.postController)
+router.route('/class/:class_id').get(classController.getClass)
 router.route('/class/create').post(classController.postClass)
 
 router.route('/class/:class_id/register').post(classController.postRegisterStudent)
