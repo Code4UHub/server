6b48b4c089bf28c3f7fcae04c4ca2f54989aa99b
JAngelGC <ja_gonzalez42@hotmail.com>
2023-04-26 20:19:26 -0600

    Refactored some endpoints

--- src/routes/student.route.ts
+++ src/routes/student.route.ts
@@ -4,5 +4,5 @@ import * as studentController from '../controllers/student.controller'
 export const router: Router = Router()
 
 router.route('/student').get(studentController.getStudents).post(studentController.postStudent)
-router.route('/student/register').post(studentController.postStudent)
+// router.route('/student/register').post(studentController.postStudent)
 router.route('/student/login').get(studentController.getStudent)
