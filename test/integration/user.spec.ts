eae4da4d354b78e7c7200a5b9af125b6f702312d
diaz <carlosd2mil@gmail.com>
2023-05-18 11:49:32 -0600

    Better integration test formatting

--- test/integration/user.spec.ts
+++ test/integration/user.spec.ts
@@ -3,7 +3,7 @@ import app from '../../src/app'
 import { Sequelize } from 'sequelize-typescript'
 import { createDb } from '../../src/database/connection'
 
-describe('GET /v1/students', () => {
+describe('GET /v1/student', () => {
   let sequelize: Sequelize
 
   beforeAll(async () => {
@@ -14,19 +14,28 @@ describe('GET /v1/students', () => {
     await sequelize.close()
   })
 
-  it('Unauthorized -> Token not provided', async () => {
-    const res = await request(app).get('/v1/student')
+  // it('Unauthorized -> Token not provided', async () => {
+  //   const res = await request(app).get('/v1/student')
+  //
+  //   expect(res.status).toBe(401)
+  //   expect(res.body).toHaveProperty('status', 'failed')
+  //   expect(res.body).toMatchObject({ data: 'Unauthorized' })
+  // })
 
-    expect(res.status).toBe(401)
-    expect(res.body).toHaveProperty('status', 'failed')
-    expect(res.body).toMatchObject({ data: 'Unauthorized' })
-  })
+  describe('Login', () => {
+    it('[Success] Valid crendetials', async () => {
+      const res = await request(app).get('/v1/student/login?email=a00000001@tec.mx&password=Abc123456')
 
-  it('Sucess -> Student login', async () => {
-    const res = await request(app).get('/v1/student/login?email=a00000001@tec.mx&password=Abc123456')
+      expect(res.status).toBe(200)
+      expect(res.body).toHaveProperty('status', 'success')
+      expect(res.body).toHaveProperty('data')
+    })
 
-    expect(res.status).toBe(200)
-    expect(res.body).toHaveProperty('status', 'success')
-    expect(res.body).toHaveProperty('data')
+    it('[Failed] Invalid email', async () => {
+      const res = await request(app).get('/v1/student/login?email=a0000001@tec.mx&password=Abc123456')
+
+      expect(res.status).toBe(400)
+      expect(res.body).toHaveProperty('status', 'failed')
+    })
   })
 })
