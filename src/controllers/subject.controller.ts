240d2b39641ba6e1383bb779985736d0f7a032c0
JAngelGC <ja_gonzalez42@hotmail.com>
2023-05-18 08:24:48 -0600

    Implemented route to create a subject

--- src/controllers/subject.controller.ts
+++ src/controllers/subject.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
-import { selectSubjects } from '../database/query/subject.query'
+import { selectSubjects, createClass } from '../database/query/subject.query'
+import { SubjectType } from '../types/subject.type'
 
 export const getSubjects = async (req: Request, res: Response) => {
   try {
@@ -10,5 +11,34 @@ export const getSubjects = async (req: Request, res: Response) => {
     })
   } catch (e) {
     throw e
+  }
+}
+
+export const postSubject = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const newSubject: SubjectType = req.body
+    const query = await createClass(newSubject)
+
+    // If class valid and code available
+    if (query !== null && typeof query === 'object') {
+      res.status(201).json({
+        status: 'success',
+        data: 'Subject created successfully'
+      })
+      return
+    }
+
+    // If class already exists
+    res.status(409).json({
+      status: 'failed',
+      data: 'Subject id already exists'
+    })
+    return
+  } catch (e: any) {
+    console.log(e)
+    res.status(500).json({
+      status: 'error',
+      data: 'Couldnt create class'
+    })
   }
 }
