a1f694c20dfa62771df69d8b2c4b4100d94789eb
José Ángel González <77761803+JAngelGC@users.noreply.github.com>
2023-05-31 23:39:04 -0600

    Feature/homework update (#59)
    * Added relation between question_h and module
    
    * Route to get question homework by module and difficulty
    
    * Route to get incoming homeworks
    
    * Route to get all questions by subject and difficulty id
    
    * Route to get scores of a homework by its id

--- src/controllers/homework.controller.ts
+++ src/controllers/homework.controller.ts
@@ -5,9 +5,10 @@ import {
   createHomework,
   createHomeworkQuestions,
   selectHomeworkQuestionsByStudent,
-  selectQuestionsByModuleAndDifficultyId
+  selectQuestionsBySubjectAndDifficultyId,
+  selectStudentScoresByClassId
+
 } from '../database/query/homework.query'
-import { HomeworkType } from '../types/homework.type'
 
 import { QuestionHType } from '../types/questionH.type'
 
@@ -24,12 +25,12 @@ export const getQuestions = async (req: Request, res: Response) => {
   }
 }
 
-export const getQuestionsByModuleAndDifficultyId = async (req: Request, res: Response) => {
-  const module_id = req.params.module_id
+export const getQuestionsBySubjectAndDifficultyId = async (req: Request, res: Response) => {
+  const subject_id = req.params.subject_id
   const difficulty_id = req.params.difficulty_id
 
   try {
-    const query = await selectQuestionsByModuleAndDifficultyId(module_id, difficulty_id)
+    const query = await selectQuestionsBySubjectAndDifficultyId(subject_id, difficulty_id)
     res.status(200).json({
       status: 'success',
       data: query
@@ -119,5 +120,19 @@ export const getHomeworkQuestions = async (req: Request, res: Response): Promise
       status: 'error',
       data: 'Couldnt get questions'
     })
+  }
+}
+
+export const getStudentScores = async (req: Request, res: Response) => {
+  const homework_id = req.params.homework_id
+
+  try {
+    const query = await selectStudentScoresByClassId(homework_id)
+    res.status(200).json({
+      status: 'success',
+      data: query
+    })
+  } catch (e) {
+    throw e
   }
 }
--- src/routes/homework.route.ts
+++ src/routes/homework.route.ts
@@ -4,10 +4,14 @@ import * as homeworkController from '../controllers/homework.controller'
 export const router: Router = Router()
 
 router
-  .route('/homework/question/module/:module_id/difficulty/:difficulty_id')
-  .get(homeworkController.getQuestionsByModuleAndDifficultyId)
+  .route('/homework/question/subject/:subject_id/difficulty/:difficulty_id')
+  .get(homeworkController.getQuestionsBySubjectAndDifficultyId)
+
 router.route('/homework/question').get(homeworkController.getQuestions)
 router.route('/homework/question').post(homeworkController.postQuestion)
 
 router.route('/homework').post(homeworkController.postHomework)
 router.route('/homework/:homework_id/student/:student_id/questions').get(homeworkController.getHomeworkQuestions)
+
+// check score for homework students
+router.route('/homework/:homework_id/student_scores').get(homeworkController.getStudentScores)
