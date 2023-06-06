7cab72fa60f08dcdfcba1b31440b7d8382e477a7
diaz <carlosd2mil@gmail.com>
2023-06-06 14:45:40 -0600

    WIP

--- src/controllers/compiler.controller.ts
+++ src/controllers/compiler.controller.ts
@@ -1,32 +1,37 @@
 import { Request, Response } from 'express'
+import { updateStudentQuestionScore } from '../database/query/studentQuestion.query'
+import { StudentQuestionType } from '../types/studentQuestion.type'
+
+const url = 'http://localhost:8080'
 
 export const runCode = async (req: Request, res: Response): Promise<void> => {
   try {
-    const url = 'http://localhost:8080/run'
     const data = JSON.stringify(req.body)
-    // console.log(data)
 
-    const result = await fetch(url, {
+    const result = await fetch(`${url}/run`, {
       method: 'POST',
+      body: data,
       headers: {
         'Content-Type': 'application/json'
-      },
-      body: data
+      }
     })
+
     const responseText = await result.json()
     console.log(responseText)
 
-    if (responseText.status == 'success') {
+    if (responseText.grade !== 100) {
+      res.status(400).json({
+        status: 'failed',
+        data: responseText
+      })
+      return
+    } else {
       res.status(200).json({
         status: 'success',
         data: responseText
       })
       return
     }
-    res.status(200).json({
-      status: 'failed',
-      data: responseText
-    })
   } catch (e: any) {
     console.log(e)
     res.status(500).json({
@@ -38,36 +43,82 @@ export const runCode = async (req: Request, res: Response): Promise<void> => {
 
 export const submitCode = async (req: Request, res: Response): Promise<void> => {
   try {
-    const url = 'http://localhost:8080/run'
-    const data = JSON.stringify(req.body)
-    console.log(data)
+    const student_id = req.body.student_id
+    const questions = req.body.questions
 
-    const result = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: data
-    })
-    const responseText = await result.json()
-    console.log(responseText.status)
+    // Submit each question of a students challenge
+    const studentQuestionsScore = questions.map(async (question: any) => {
+      console.log(question.question_id)
+      if (question.type === 'open') {
+        console.log('----------------')
+        // console.log(question)
 
-    if (result.status === 200) {
-      res.status(200).json({
-        status: 'success',
-        data: responseText
-      })
-      return
-    }
-    res.status(400).json({
-      status: 'failed',
-      data: responseText
+        const data = JSON.stringify(question)
+        // const data = JSON.stringify({
+        //   question_id: question.question_id,
+        //   source_code: question.source_code,
+        //   shown_tests: question.shown_tests
+        // })
+
+        const result = await fetch(`${url}/submit`, {
+          method: 'POST',
+          body: data,
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+
+        const responseText = await result.json()
+        return responseText
+      } else if (question.type === 'close') {
+        console.log('That')
+      } else {
+        console.log('Question type not valid')
+      }
     })
+
+    Promise.all(studentQuestionsScore).then((data) => {
+      console.log('================')
+      console.log(data)
+    })
+
+    // const result = await fetch(url, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: source_code
+    // })
+    // const responseText = await result.json()
+    // console.log(responseText)
+
+    // if (responseText.status == 'error') {
+    //   res.status(400).json({
+    //     status: 'failed',
+    //     data: responseText
+    //   })
+    //   return
+    // } else {
+    //   res.status(200).json({
+    //     status: 'failed',
+    //     data: responseText
+    //   })
+    //   return
+    // }
+    // updateStudentQuestionScore(student_question)
+
+    res.status(200).json({
+      status: 'success',
+      data: 'data goes here'
+    })
+    return
+    // Update students submission with current code
+    // call query that updates a score
   } catch (e: any) {
     console.log(e)
     res.status(500).json({
       status: 'error',
-      data: 'Couldnt submit your code'
+      data: 'Couldnt run your code'
     })
   }
 }
--- src/routes/compiler.route.ts
+++ src/routes/compiler.route.ts
@@ -3,5 +3,5 @@ import * as compilerController from '../controllers/compiler.controller'
 
 export const router: Router = Router()
 
-router.route('/run').post(compilerController.runCode)
-router.route('/submit').post(compilerController.submitCode)
+router.route('/run').put(compilerController.runCode)
+router.route('/submit').put(compilerController.submitCode)
