22f2d9a0cddae02ee4f89c893363263ac480b22d
diaz <carlosd2mil@gmail.com>
2023-06-13 22:28:19 -0600

    Init update

--- init.sh
+++ init.sh
@@ -6,4 +6,4 @@ docker build -t code4u-server:1.0 .
 echo -e "\nStarting container"
 docker kill code4u-server >/dev/null 2>&1
 docker rm code4u-server >/dev/null 2>&1
-docker run -d -p 9050:8000 --restart unless-stopped --name server code4u-server:1.0 
+docker run -d -p 9150:8000 --restart unless-stopped --name server code4u-server:1.0 
